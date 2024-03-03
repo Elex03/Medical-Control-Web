45e29694efa15310e6ab9c97a1f294b18870284d
D-Maysena <yassercomunica@gmail.com>
2024-03-02 22:26:38 -0600

    index or main page partially finished

--- app.py
+++ app.py
@@ -8,3 +8,15 @@ Scss (app)
 @app.route('/')
 def index():
     return render_template('login.html')
+
+@app.route('/pacientes', methods=["POST"])
+def pacientes():
+    
+    pacientes = [
+    {"name": "Sofia Ramirez Moreno", "id": "001-090205-1003N"},
+    {"name": "Javier Fernandez Gomez", "id": "001-090205-1003N"},
+    {"name": "Ana Martinez Rodriguez", "id": "001-090205-1003N"}
+    ]
+    
+
+    return render_template('index.html', pacientes=pacientes)
